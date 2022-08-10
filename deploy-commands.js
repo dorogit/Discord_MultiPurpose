cfdb487d23526b31a909902f89073cefcd6a77e0
Ishaan <84321539+dorogit@users.noreply.github.com>
2022-08-10 13:33:46 +0530

    Add files via upload

---
+++ deploy-commands.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder, Routes } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const { clientId, guildId, token } = require('./config.json');
+
+const commands = [
+	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
+	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+	new SlashCommandBuilder().setName('avatar').setDescription('Replies with user avatar!'),
+	new SlashCommandBuilder().setName('spam').setDescription('spams the next line of text send by user'),
+	new SlashCommandBuilder().setName('play').setDescription('play a song from YouTube'),
+	new SlashCommandBuilder().setName('creator').setDescription('Shows the creator of this bot')
+]
+	.map(command => command.toJSON());
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
